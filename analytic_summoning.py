e3bb67c21a3b6a90b60bcfb29ae77cf13ff9aa9c
unknown <ahorn508@gmail.com>
2019-11-09 14:33:02 -0500

    commented it all out because it will depend entirely on the constructor

---
+++ analytic_summoning.py
@@ -0,0 +1,32 @@
+# import argparse
+# import numpy as np
+# import json
+
+# with open("pools.json", "w") as f:
+#     pools = json.load(f)
+
+# parser = argparse.ArgumentParser(description='Determine the expected number of pulls to achieve a desired result.')
+# parser.add_argument('-t', '--tenpull', action='store_true',
+#                     help='Signifies the exclusive use of 10pulls.')
+# parser.add_argument('-nb', '--newbanner', action='store_true',
+#                     help='Indicates that this is a banner with units not on the wiki.')
+
+# args = parser.parse_args()
+
+
+# # 1pulls
+# # not a gala
+# # single desired unit
+
+# class Summoner:
+#     def __init__(self):
+#         num_focus = 1
+#         num_fivestar = 
+# num_focus = 1
+# # num_fivestar = .
+# # num_fourstar = .
+# # num_threestar = .
+# init_rate = .04
+# pity_inc = .005
+# pity_breaker = (.1 - init_rate)/pity_inc
+# stoch_mat = np.zeros(101)
