e5c093e2065e26849129a6280a2686b9d8f4b6b1
unknown <ahorn508@gmail.com>
2019-11-13 14:33:40 -0500

    fixed inaccuracies for single targets, arbitrary intersections are still incorrect

--- constructors.py
+++ constructors.py
@@ -1,9 +1,10 @@
 import numpy as np
 import itertools
 import decimal
-from decimal import Decimal
 import pprint
 import time
+Dec = decimal.Decimal
+decimal.getcontext()
 
 class BlockGenerator:
     def __init__(self, wants):
@@ -16,7 +17,7 @@ class BlockGenerator:
         for i in range(0, len(self.universe)):
             for j in itertools.combinations(self.universe, i):
                 self.indices.append(frozenset(j))
-        
+
         self.bloc_struc = np.array([])
         self.end_col = np.array([])
         self.precomp_blocks()
@@ -25,6 +26,7 @@ class BlockGenerator:
             need = self.universe - vertical
             gained_list = {}
             final = self.endmap[need]
+            final = self.blockmap[need]
             for horizontal in reversed(self.indices):
                 gain = horizontal - vertical
                 if vertical == horizontal:
@@ -35,9 +37,11 @@ class BlockGenerator:
                     for prehandled in gained_list.keys():
                         if prehandled > horizontal:
                             block -= gained_list[prehandled]
-                    gained_list[horizontal] = block      
+                    gained_list[horizontal] = block
                 else:
                     block = self.blockmap[frozenset()]
+                # if vertical == frozenset():
+                #     print(block)
                 try:
                     horz = np.hstack((block, horz))
                 except ValueError:
@@ -93,8 +97,8 @@ class BlockGenerator:
                     if self.wants[unit]['rarity'] == 5:
                         p_5_correct.append((self.wants[unit]['base prob'] + pity_level*self.wants[unit]['prob inc']))
                 p_forced = p_norm
-                p_diag = (p_norm - ((.04 + pity_level*.005) - sum(p_5_correct)))**9
-                p_diag *= (p_alt - ((.04 + pity_level*.005) - sum(p_5_correct)))
+                p_diag = (p_norm - ((Dec('.04') + pity_level*Dec('.005')) - sum(p_5_correct)))**9
+                p_diag *= (p_alt - ((Dec('.04') + pity_level*Dec('.005')) - sum(p_5_correct)))
                 #placeholder vals
             else:
                 p_forced = 1
@@ -114,14 +118,13 @@ class BlockGenerator:
             n_vec = []
             spec_vec = []
             order = []
-            no_p = 1
-            no_p_alt = 1
+            no_p = Dec('1')
+            no_p_alt = Dec('1')
             flag_5 = False
-            # print(acquired)
             for unit in acquired:
                 order.append(unit)
                 p_vec.append(self.wants[unit]['base prob'])
-                n_vec.append(1)
+                n_vec.append(Dec('1'))
                 spec_vec.append(self.wants[unit]['prob inc'])
                 no_p -= self.wants[unit]['spec prob']
                 no_p_alt -= self.wants[unit]['alt prob']
@@ -134,21 +137,18 @@ class BlockGenerator:
                         flag_5 = True
                 for i in range(0, len(p_vec)):
                     new_n = n_vec[:i] + [0] + n_vec[i+1:]
-                    p_break += self.wants[order[i]]['alt prob']*self.recursive_p(p_vec, new_n, sum(new_n), pull_size=9)
-                # print(p_vec)
-                # print(n_vec)
+                    p_break += (self.wants[order[i]]['alt prob'] + pity_level*self.wants[order[i]]['alt inc'])*self.recursive_p(p_vec, new_n, sum(new_n), pull_size=9)
                 p_break += no_p_alt*self.recursive_p(p_vec, n_vec, sum(n_vec), pull_size=9)
                 if flag_5:
                     return p_break, p_diag
                 else:
                     p_get = 0
-                    p_vec_alt = p_vec + [.04 + pity_level*.005]
+                    p_vec_alt = p_vec + [Dec('.04') + pity_level*Dec('.005')]
                     n_vec_alt = n_vec + [1]
                     for i in range(0, len(p_vec_alt)):
                         new_n = n_vec_alt[:i] + [0] + n_vec_alt[i+1:]
-                        ## ERROR HERE
                         if i < len(p_vec):
-                            p_get += self.wants[order[i]]['alt prob']*self.recursive_p(p_vec_alt, new_n, sum(new_n), pull_size=9)
+                            p_get += (self.wants[order[i]]['alt prob'] + pity_level*self.wants[order[i]]['alt inc'])*self.recursive_p(p_vec_alt, new_n, sum(new_n), pull_size=9)
                         else:
                             p_get += p_vec_alt[-1]*self.recursive_p(p_vec_alt, new_n, sum(new_n), pull_size=9)
                     p_get += no_p_alt*self.recursive_p(p_vec_alt, n_vec_alt, sum(n_vec_alt), pull_size=9)
@@ -159,49 +159,72 @@ class BlockGenerator:
                 for i in range(0, len(p_vec)):
                     new_n = n_vec[:i] + [0] + n_vec[i+1:]
                     for j in range(0, len(p_vec)):
-                        if j >= len(p_vec)-1:
-                            p_break += self.wants[order[i]]['spec prob']*no_p_alt*self.recursive_p(p_vec, new_n, sum(new_n), pull_size=8)
-                        else:
-                            new_n_alt = new_n[:j] + [0] + new_n[j+1:]
-                            p_break += self.wants[order[i]]['spec prob']*self.wants[order[j]]['alt prob']*self.recursive_p(p_vec, new_n_alt, sum(new_n_alt), pull_size=8)
+                        new_n_alt = new_n[:j] + [0] + new_n[j+1:]
+                        p_break += self.wants[order[i]]['spec prob']*self.wants[order[j]]['alt prob']*self.recursive_p(p_vec, new_n_alt, sum(new_n_alt), pull_size=8)
+                    p_break += self.wants[order[i]]['spec prob']*no_p_alt*self.recursive_p(p_vec, new_n, sum(new_n), pull_size=8)
                 for k in range(0, len(p_vec)):   
                     new_n_fin = n_vec[:k] + [0] + n_vec[k+1:]
                     p_break += no_p*self.wants[order[k]]['alt prob']*self.recursive_p(p_vec, new_n_fin, sum(new_n_fin), pull_size=8)
                 p_break += no_p*no_p_alt*self.recursive_p(p_vec, n_vec, sum(n_vec), pull_size=8)
                 return p_break, p_diag
     
+
     def choose(self, n, r):
         f = self.factorial
-        return f(n)//(f(r)*f(n-r))
+        return f(n)//f(r)//f(n-r)
+
+    def multinom(self, n, m):
+        if m >= sum(n):
+            n += [m - sum(n)]
+            denom = 1
+            num = self.factorial(m)
+            for i in range(0, len(n)):
+                denom *= self.factorial(n[i])
+            return num//denom
+        else:
+            print('multinomial size error')
+            exit()
 
     def factorial(self, n):
-        if n <= 1:
+        if n == 0:
             return 1
-        else:
+        elif n > 0:
             return n*self.factorial(n-1)
+        else:
+            print('Something Broke')
+            exit()
 
     def recursive_p(self, p, n, m, pull_size=10):
         if m == pull_size:
             _p = 1
             for i in range(0, len(p)):
                 _p *= p[i]**n[i]
-            return _p*pull_size
+            return _p*self.choose(pull_size, m)
+            # return _p*self.multinom(n + [pull_size - m], pull_size)
         else:
             _p = 1
             for i in range(0, len(p)):
                 _p *= p[i]**n[i]
             end_p = _p*self.choose(pull_size, m)*(1-sum(p))**(pull_size-m)
+            # end_p = _p*self.multinom(n + [pull_size - m], pull_size)
             for i in range(0, len(p)):
                 new_n = n[:i] + [n[i]+1] + n[i+1:]
                 end_p += self.recursive_p(p, new_n, m+1, pull_size=pull_size)
             return end_p
 
     def test_pt_matrix(self):
+        # for i in range(0, len(self.bloc_struc)):
+        #     print(sum(self.bloc_struc[i]))
+        # print('-----')
+        # for i in range(0, len(self.bloc_struc)):
+        #     print(sum(self.end_col[i]))
+        # print('-----')
+        print('errors:')
+        err = []
         for i in range(0, len(self.bloc_struc)):
-            print(f'{sum(self.bloc_struc[i])}')
-        print('-----')
-        for i in range(0, len(self.bloc_struc)):
-            print(sum(self.megablock[i]))
+            err.append(1 - sum(self.megablock[i]))
+            print(err[i])
+        print(f'Sum of Errors: {sum(err)}')
             # if sum(self.bloc_struc[i]) != 1:
             #     print(f'error on row {i} : sums to {sum(self.bloc_struc[i])}')
     def hitting_time(self):
@@ -215,37 +238,37 @@ class BlockGenerator:
     
 grundlespite = {
     'Akasha': {
-        'base prob' : .005,
-        'alt prob' : .005,
-        'spec prob' : .125,
-        'prob inc' : .000639,
-        'alt inc' : .000639,
+        'base prob' : Dec('.005'),
+        'alt prob' : Dec('.005'),
+        'spec prob' : Dec('.125'),
+        'prob inc' : Dec('.000639'),
+        'alt inc' : Dec('.000639'),
         'rarity' : 5
         },
     'WXania' : {
-        'base prob' : .02333,
-        'alt prob' : .14001,
-        'spec prob' : 0,
-        'prob inc' : 0,
-        'alt inc' : -.00073,
+        'base prob' : Dec('.02333'),
+        'alt prob' : Dec('.14001'),
+        'spec prob' : Dec('0'),
+        'prob inc' : Dec('0'),
+        'alt inc' : Dec('-.00073'),
         'rarity' : 4
         }
     }
 
 s_time = time.process_time()
-
-test = BlockGenerator(grundlespite)
-print('generated:')
-print(time.process_time() - s_time)
-# test.test_pt_matrix()
 # np.set_printoptions(precision=3)
+test = BlockGenerator(grundlespite)
+# print('generated:')
+# print(time.process_time() - s_time)
+test.test_pt_matrix()
+np.set_printoptions(precision=3)
 # printer = pprint.PrettyPrinter()
 # printer.pprint(test.endmap)
 # print(test.universe)
 # print(test.fullsize)
-# print(test.bloc_struc)
-# print(test.end_col)
+print(test.bloc_struc)
+print(test.end_col)
 test.hitting_time()
-print('inverted:')
-print(time.process_time() - s_time)
+# print('inverted:')
+# print(time.process_time() - s_time)
 
